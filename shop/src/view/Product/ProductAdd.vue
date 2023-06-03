a0f100d8e03707bb1ebca39545544f671d21babb
YangCzcp <120726520+YangCzcp@users.noreply.github.com>
2023-06-03 15:29:30 +0800

    1

--- shop/src/view/Product/ProductAdd.vue
+++ shop/src/view/Product/ProductAdd.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-      <h1>商品发布</h1>
-    </div>
-  </template>
-
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>新增商品</el-breadcrumb-item>
+    </el-breadcrumb>
+ 
+    <!-- 卡片视图区域 -->
+   
+    
+ 
+     
+ 
+      <!-- 分页区域 -->
+      
+  </div>
+</template>
+ 
 <script>
 
 </script>
-<style>
-
+ 
+<style lang="less" scoped>
 </style>
--- shop/src/view/Product/ProductList.vue
+++ shop/src/view/Product/ProductList.vue
@@ -1,12 +1,81 @@
 <template>
-    <div>
-      <h1>这是个商品列表</h1>
-    </div>
-  </template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+ 
+    <!-- 卡片视图区域 -->
+    <el-card class="box-card">
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" >
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" >添加商品</el-button>
+        </el-col>
+      </el-row>
+ 
+      <!-- table表格区域 -->
+      <el-table>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格(元)" prop="goods_price" width="95px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="80px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140px">
+          <template slot-scope="scope">
+            <!-- {{}} -->
+       </template>
+      </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" ></el-button>
+          </template>
+        </el-table-column>
+        
+      </el-table>
 
+      
+
+  
+    
+ 
+
+ 
+      <!-- 分页区域 -->
+      <el-pagination>
+      </el-pagination>
+    </el-card>
+  </div>
+</template>
+ 
 <script>
 
-</script>
-<style>
+export default {
+  data() {
+    return {
+     
+    }
+  },
+  created() {
+   
+  },
+  methods: {
+    
+  }
 
+}
+</script>
+ 
+<style lang="less" scoped>
+
+  .box-card {
+    // width: 480px;
+    margin: 15px auto;
+  }
 </style>
